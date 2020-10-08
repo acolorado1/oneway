5cdf8aa523f5c7e6aa8552dd291732b372f0ba71
Sofia Colorado <acolorado@wesleyan.edu>
2020-10-08 16:01:43 -0400

    initial commit

---
+++ R/oneway.R
@@ -0,0 +1,32 @@
+#' One Way Analysis of Variance
+#'
+#' oneway computes a one-way analysis of variance
+#' and includes group level summary statistics
+#'
+#' @param formula an object of class formula relating the
+#' dependent variable to the grouping variable.
+#' @param data a data frame containing the varibles in the model.
+#'
+#' @export
+#'
+#' @return a list with two elements.
+#' @examples
+#' mileage <- oneway(mpg ~ cyl, mtcars)
+oneway <- function(formula, data){
+  #delete missing data
+  data <- na.omit(data)
+
+  #ANOVA
+  fit <- lm(formula, data)
+
+  #summary statistics
+  stats <- aggregate(formula, data,
+                     function(x) c(n= length(x),
+                                   mean= mean(x),
+                                   sd = sd(x)))
+  #return results
+  result <- list(anova = fit, summarystats = stats)
+  class(result) <- "oneway"
+  return(result)
+
+}
